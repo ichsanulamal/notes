f2457140a0f624b13211443b798abb530b99fcb8
ichsanulamal <muhammad.ichsanul19@gmail.com>
2024-07-29 12:50:59 +0700

    pct

--- dbt_pcomp/models/pcomp.sql
+++ dbt_pcomp/models/pcomp.sql
@@ -1,5 +1,11 @@
-
-select * from (select * from {{ ref('enterkomputer') }}
+with final as (
+    select * from (select * from {{ ref('enterkomputer') }}
 union all
 select * from {{ ref('viraindo') }})
 QUALIFY ROW_NUMBER() OVER (PARTITION BY product_name, category, price, `source` ORDER BY inserted_at ASC nulls last) = 1
+)
+
+select *,
+    ((price - LAG(price) OVER (PARTITION BY product_name, category, source ORDER BY inserted_at)) / LAG(price) OVER (PARTITION BY product_name, category, source ORDER BY inserted_at)) * 100 AS percentage_change
+from final 
+
